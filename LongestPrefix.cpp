7ee4e22a86faca5e769faf815f2ce6ed3e6f1834
Marston Yu <marstonyu@outlook.com>
2019-02-07 23:32:03 +0800

    Vertical scanning.

--- LongestPrefix.cpp
+++ LongestPrefix.cpp
@@ -8,40 +8,16 @@ string longestCommonPrefix(vector<string> &strs) {
   if (strs.size()==0) return "";
   if (strs.size()==1) return strs[0];
 
-  // sort vector for better performance.
-  struct {
-    bool operator()(string a, string b) const
-    {
-      return a.size() < b.size();
-    }
-  } customLess;
-  sort(strs.begin(), strs.end(), customLess);
+  // vertical scanning. Increasing array index and compare chars in same position.
+  for (int i = 0; i < strs[0].size(); i++) {
 
-  string common = "";
-  string pattern = "";
-  string content = "";
-  for (int i = 0; i < strs.size() - 1; i++) {
-    // compare the first and second string at the beginning
-    // while compare next string with common prefix in future iteration.
-    if(i == 0) {
-      pattern = strs[i].size() < strs[i + 1].size() ? strs[i] : strs[i + 1];
-      content = strs[i].size() < strs[i + 1].size() ? strs[i + 1] : strs[i];
-    } else {
-      pattern = common;
-      content = strs[i+1];
-    }
-    // shrink the pattern to find the common prefix of the pattern and content strings.
-    for (int j = pattern.size(); j > 0; j--) {
-      if (content.substr(0, j) == pattern.substr(0, j)) {
-        common = pattern.substr(0, j);
-        break;
+    for (int j = 0; j < strs.size(); j++) {
+      // When two chars not matching, string before the unmatched chars is the longest common prefix.
+      if (strs[0][i]!=strs[j][i]) {
+        return strs[0].substr(0, i);
       }
-      // set common to empty when pattern and content have no common prefix
-      // common prefix in last iteration has been stored in pattern variable.
-      common = "";
     }
-    // finish searching when pattern and content have no common prefix.
-    if(common == "") break;
   }
-  return common;
+  // if all strings are equal, return one of those.
+  return strs[0];
 }
