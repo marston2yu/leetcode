41b2a74fc1eda75639ed7385004d556404b8c6ae
Marston Yu <marstonyu@outlook.com>
2019-07-09 15:48:53 +0800

    Memory optimize.

--- UniquePathsII.cpp
+++ UniquePathsII.cpp
@@ -8,33 +8,35 @@ int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid) {
   if (m == 1 || n == 1) return 1;
 
   // memoization of how many path are there from start to (i, j).
-  vector<vector<unsigned>> P(m, vector<unsigned>(n, 1)); // use unsigned integer to avoid overflow.
+  // using obstacleGrid as memo to optimize memory consumption.
 
   // initialize first row.
+  obstacleGrid[0][0] = 1;
   for (int j = 1; j < n; ++j) {
-    if (obstacleGrid[0][j] == 0 && P[0][j - 1] == 1) {
-      P[0][j] = 1;
+    if (obstacleGrid[0][j] == 0 && obstacleGrid[0][j - 1] == 1) {
+      obstacleGrid[0][j] = 1;
     } else {
-      P[0][j] = 0;
+      obstacleGrid[0][j] = 0;
     }
   }
 
 
   for (int i = 1; i < m; ++i) {
     // initialize first column.
-    if (obstacleGrid[i][0] == 0 && P[i - 1][0] == 1) {
-      P[i][0] = 1;
+    if (obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1) {
+      obstacleGrid[i][0] = 1;
     } else {
-      P[i][0] = 0;
+      obstacleGrid[i][0] = 0;
     }
 
     for (int j = 1; j < n; ++j) {
       if (obstacleGrid[i][j] == 1) {
-        P[i][j] = 0;
+        obstacleGrid[i][j] = 0;
       } else {
-        P[i][j] = P[i - 1][j] + P[i][j - 1];
+        // enforce type conversion to avoid signed integer overflow.
+        obstacleGrid[i][j] = (unsigned) obstacleGrid[i - 1][j] + (unsigned) obstacleGrid[i][j - 1];
       }
     }
   }
-  return P[m - 1][n - 1];
+  return obstacleGrid[m - 1][n - 1];
 }
