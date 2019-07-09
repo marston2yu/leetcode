d731052a13eaa0b408c2923d176c019bd85b66a8
Marston Yu <marstonyu@outlook.com>
2019-07-09 15:57:56 +0800

    Memo how many path from (i, j) to target.

--- UniquePathsII.cpp
+++ UniquePathsII.cpp
@@ -7,36 +7,36 @@ int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid) {
   if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) return 0; // start or target is blocked.
   if (m == 1 || n == 1) return 1;
 
-  // memoization of how many path are there from start to (i, j).
+  // memoization of how many path are there from (i, j) to target.
   // using obstacleGrid as memo to optimize memory consumption.
 
-  // initialize first row.
-  obstacleGrid[0][0] = 1;
-  for (int j = 1; j < n; ++j) {
-    if (obstacleGrid[0][j] == 0 && obstacleGrid[0][j - 1] == 1) {
-      obstacleGrid[0][j] = 1;
+  // initialize last row.
+  obstacleGrid[m - 1][n - 1] = 1;
+  for (int j = n - 2; j >= 0; --j) {
+    if (obstacleGrid[m - 1][j] == 0 && obstacleGrid[m - 1][j + 1] == 1) {
+      obstacleGrid[m - 1][j] = 1;
     } else {
-      obstacleGrid[0][j] = 0;
+      obstacleGrid[m - 1][j] = 0;
     }
   }
 
 
-  for (int i = 1; i < m; ++i) {
-    // initialize first column.
-    if (obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1) {
-      obstacleGrid[i][0] = 1;
+  for (int i = m - 2; i >= 0; --i) {
+    // initialize last column.
+    if (obstacleGrid[i][n - 1] == 0 && obstacleGrid[i + 1][n - 1] == 1) {
+      obstacleGrid[i][n - 1] = 1;
     } else {
-      obstacleGrid[i][0] = 0;
+      obstacleGrid[i][n - 1] = 0;
     }
 
-    for (int j = 1; j < n; ++j) {
+    for (int j = n - 2; j >= 0; --j) {
       if (obstacleGrid[i][j] == 1) {
         obstacleGrid[i][j] = 0;
       } else {
         // enforce type conversion to avoid signed integer overflow.
-        obstacleGrid[i][j] = (unsigned) obstacleGrid[i - 1][j] + (unsigned) obstacleGrid[i][j - 1];
+        obstacleGrid[i][j] = (unsigned) obstacleGrid[i + 1][j] + (unsigned) obstacleGrid[i][j + 1];
       }
     }
   }
-  return obstacleGrid[m - 1][n - 1];
+  return obstacleGrid[0][0];
 }
