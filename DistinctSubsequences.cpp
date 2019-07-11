1c042b0e8356fcb6d196cf2f6ab0cd4501971561
Marston Yu <marstonyu@outlook.com>
2019-07-11 20:18:39 +0800

    1D memoization implementation.

--- DistinctSubsequences.cpp
+++ DistinctSubsequences.cpp
@@ -5,18 +5,19 @@ int numDistinct(string s, string t) {
   int n = t.length();
   if (m < n) return 0;
 
-  vector<vector<int>> ds(m, vector<int>(n, 0));
-  ds[0][0] = (int) (s[0]==t[0]);
+  vector<unsigned> ds(n);
+  ds[0] = (int) (s[0]==t[0]);
   for (int i = 1; i < m; ++i) {
-    ds[i][0] = ds[i - 1][0] + (s[i]==t[0] ? 1 : 0);
+    int leftUp = ds[0]; // left upper element in original 2d memo array.
+    ds[0] = leftUp + (s[i]==t[0] ? 1 : 0);
     for (int j = 1; j < n; ++j) {
+      int tmp = ds[j];
       if (s[i]==t[j]) {
-        ds[i][j] = ds[i - 1][j - 1] + ds[i - 1][j];
-      } else {
-        ds[i][j] = ds[i - 1][j];
+        ds[j] = leftUp + ds[j];
       }
+      leftUp = tmp;
     }
 
   }
-  return ds[m - 1][n - 1];
+  return ds[n - 1];
 }
