d74afce6e837b2357a0ff2a36c88dcfbdf20233f
Marston Yu <marstonyu@outlook.com>
2019-07-25 19:40:34 +0800

    Stone game solution with 1D array.

--- StoneGame.cpp
+++ StoneGame.cpp
@@ -2,25 +2,26 @@
 
 bool stoneGame(vector<int> &piles) {
   int n = piles.size();
-  vector<vector<pair<int, int>>> maxP;
-  for (int i = 0; i < n; i++) {
-    vector<pair<int, int>> v(n, make_pair(0, 0));
-    maxP.push_back(v);
-  }
+  vector<pair<int, int>> maxP(n, make_pair(0, 0));
 
-  maxP[n - 1][n - 1].first = piles[n - 1];
-  maxP[n - 1][n - 1].second = 0;
+  // Store maximum point one can get with pile[i...j].
+  // First is the point that one can get on offensive.
+  // Second is the point that one can get on defensive.
+  maxP[n - 1].first = piles[n - 1];
+  maxP[n - 1].second = 0;
 
   for (int i = n - 2; i >= 0; i--) {
-    maxP[i][i].first = piles[i];
-    maxP[i][i].second = 0;
+    maxP[i].first = piles[i];
+    maxP[i].second = 0;
     for (int j = i + 1; j < n; j++) {
-      int maxLeft = piles[i] + maxP[i + 1][j].second;
-      int maxRight = piles[j] + maxP[i][j - 1].second;
-      maxP[i][j].first = max(maxLeft, maxRight);
-      maxP[i][j].second =
-          maxLeft > maxRight ? maxP[i + 1][j].first : maxP[i][j - 1].first;
+      int maxLeft = piles[i] + maxP[j].second;
+      int maxRight = piles[j] + maxP[j - 1].second;
+      maxP[j].first = max(maxLeft, maxRight);
+      // When the offensive side determines its move, the defensive side becomes offensive side
+      // with remaining piles.
+      maxP[j].second = maxLeft > maxRight ? maxP[j].first : maxP[j - 1].first;
     }
   }
-  return maxP[0][n - 1].first - maxP[0][n - 1].second > 0;
+  // Return whether the offensive side will win.
+  return maxP[n - 1].first - maxP[n - 1].second > 0;
 }
