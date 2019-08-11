29fd0792d843bde8c11291aed2a839cdc1264c63
Marston Yu <marstonyu@outlook.com>
2019-08-11 18:05:13 +0800

    Script to generate templates

---
+++ init.py
@@ -0,0 +1,45 @@
+from argparse import ArgumentParser
+from os.path import isfile
+from sys import exit
+import re
+
+parser = ArgumentParser()
+parser.add_argument("-n", "--name", dest="filename", help="generated file name")
+args = parser.parse_args()
+
+
+header_filename = args.filename + ".h"
+cpp_filename = args.filename + ".cpp"
+test_filename = args.filename + "Test.cpp"
+
+if isfile(header_filename):
+  exit("File exists: {}".format(header_filename))
+
+if isfile(cpp_filename):
+  exit("File exists: {}".format(cpp_filename))
+
+if isfile(test_filename):
+  exit("File exists: {}".format(test_filename))
+
+with open(header_filename, 'w') as f:
+  template = '''#ifndef LEETCODE_{0}_H
+#define LEETCODE_{0}_H
+#endif //LEETCODE_{0}_H'''.format(args.filename.upper())
+  f.write(template)
+
+
+with open(cpp_filename, 'w') as f:
+  template = '#include "{}"\n'.format(header_filename)
+  f.write(template)
+
+with open(test_filename, 'w') as f:
+  testname = ' '.join(re.findall('[A-Z][^A-Z]*', args.filename))
+  template = '''#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include "{0}"
+TEST_CASE("Test {1}") {{
+  REQUIRE();
+}}'''.format(header_filename, testname)
+  f.write(template)
+  
+
